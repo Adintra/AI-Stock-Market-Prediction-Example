fdf963050bc5f6553b11fa61e5230f92e4a2a0b7
Adam Worrall <aworrall333@gmail.com>
2019-12-17 02:10:40 +0000

    Created gui class inheriting from Tkinter. GUI is going to have a class per page inheriting from Tkinter frame. Skeleton of start page created.

--- Interface.py
+++ Interface.py
@@ -1,4 +1,5 @@
 import matplotlib
+
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
@@ -8,17 +9,35 @@ import numpy as np
 import tkinter as tk
 from tkinter import ttk
 
+
+class AIStockMarketGUI(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}  # Used for multiple windows in app
+        frame = StartPage(container, self)
+        self.frames[StartPage] = frame
+        frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame(StartPage)
+
+    def show_frame(self, controller):
+        frame = self.frames[controller]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)  # Parent is AIStockMarketGUI
+        label = tk.Label(self, text="Start Page", font=("Verdana", 12))
+        label.pack(pady=10, padx=10)
+
+
 if __name__ == "__main__":
-    root = tk.Tk()
-    figure = Figure(figsize=(5, 4), dpi=100)
-    plot = figure.add_subplot(1, 1, 1)
-
-    x = np.linspace(0.0, 2*2*3.14159, 100)
-    y = np.sin(x)
-
-    plot.plot(x, y, color="blue", marker="x", linestyle="")
-
-    canvas = FigureCanvasTkAgg(figure, root)
-    canvas.get_tk_widget().grid(row=0, column=0)
-
-    root.mainloop()
+    gui = AIStockMarketGUI()
+    gui.mainloop()
