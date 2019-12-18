56b6a41e35bfb2e389dd3f1716c64e18d9d3b286
Adam Worrall <aworrall333@gmail.com>
2019-12-18 13:51:19 +0000

    Applied data normalization ready for AI training.

--- AIModel.py
+++ AIModel.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 # Plotting datetime with matplotlib
 from pandas.plotting import register_matplotlib_converters
@@ -27,6 +28,29 @@ class AIDataLoader:
         self.df.dropna(inplace=True)  # Drop missing values
         self.df_dropped_length = len(self.df)
         self.data = pd.DataFrame(self.df[' Close/Last'])  # Column names have extra space at start.
+        self.data[self.data.columns[0]] = self.data[self.data.columns[0]].apply(lambda x: x.replace(' $', ''))
+
+        # Prepare data for train/test/eval
+        self.data_values = self.data[' Close/Last'].values.astype(float)
+        self.test_size = 12
+        # Train and test sets
+        self.train_set = self.data_values[:-self.test_size]
+        self.test_set = self.data_values[-self.test_size:]
+
+        # Normalize data
+        self.scaler = MinMaxScaler(feature_range=(-1, 1))
+        self.train_norm = self.scaler.fit_transform(self.train_set.reshape(-1, 1))
+        self.train_norm = torch.FloatTensor(self.train_norm).view(-1)
+        self.train_data = self.input_data(window_size=12)
+
+    def input_data(self, window_size):
+        out = []
+        length = len(self.train_norm)
+        for i in range(length-window_size):
+            window = self.train_norm[i:i+window_size]
+            label = self.train_norm[i+window_size:i+window_size+1]
+            out.append((window, label))
+        return out
 
 
 if __name__ == "__main__":
@@ -35,3 +59,5 @@ if __name__ == "__main__":
     print(tesla_dataloader.df_initial_length)
     print(tesla_dataloader.df_dropped_length)
     print(tesla_dataloader.data.head())
+    print(tesla_dataloader.test_set)
+    print(tesla_dataloader.train_data[0])
